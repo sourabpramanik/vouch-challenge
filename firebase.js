574a538382867ed043dc8086d97b09544c055d04
Sourab <shubpramanik241@gmail.com>
2022-01-05 17:31:03 +0530

    firebase auth setup

---
+++ firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+
+const config = {
+  apiKey: "AIzaSyDcqkflIGIn60hnkClW7JhKkLBBVhhiuAw",
+  authDomain: "quickstart-1607022885703.firebaseapp.com",
+  projectId: "quickstart-1607022885703",
+  storageBucket: "quickstart-1607022885703.appspot.com",
+  messagingSenderId: "985223638703",
+  appId: "1:985223638703:web:69a5e6195952f38d6aee5a",
+  measurementId: "G-0C1GJRR84J"
+};
+
+try {
+    initializeApp(config)
+} catch (error) {
+    console.log(error);    
+}
+
+const app = getApp();
+const auth = getAuth();
+
+export { app, auth }
--- src/screens/sign-up.tsx
+++ src/screens/sign-up.tsx
@@ -4,74 +4,118 @@ import InputField from '../components/input-field';
 import CustomButton from '../components/custom-button';
 import TextMastHead from '../components/text-masthead';
 import AnimatedModal from '../components/animated-modal';
+import { FirebaseRecaptchaVerifierModal, FirebaseRecaptchaBanner } from "expo-firebase-recaptcha";
+import { getAuth, PhoneAuthProvider, signInWithCredential } from 'firebase/auth';
+import { app, auth } from '../../firebase';
+
+const initialValue = {
+    first_name: '',
+    last_name: '',
+    username: '',
+    email: '',
+    phone: '',
+    referral: '',
+    verificationId: '',
+}
 
 export default function SignUpPage(){
+    
+    // Hooks
+    const [formState, setFormState] = useState(initialValue)
     const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    
+    // Firebase 
+    const recaptchaVerifier = React.useRef(null);
+    const firebaseConfig = app.length ? app.options : undefined;
+
+    // Functions
     const handleModalOverlay = () =>{
         setOpen(prev => !prev)
     }
 
+    const handleGetOtp = async() =>{        
+        try {
+            setLoading(true)
+            const phoneProvider = new PhoneAuthProvider(auth);
+            const verificationId = await phoneProvider.verifyPhoneNumber(
+              formState.phone,
+              recaptchaVerifier.current
+            );
+            setFormState({...formState, verificationId: verificationId});            
+        } catch (error) {
+            setLoading(false)
+            console.log(error);            
+        }                
+    }
+    console.log(formState);
+    
     return(
         <VStack flex={1} space={2} bg="white">
             <TextMastHead title="Sign Up!"/>
             <VStack>
+                <FirebaseRecaptchaVerifierModal
+                    ref={recaptchaVerifier}
+                    firebaseConfig={app.options}
+                    attemptInvisibleVerification
+                />
                 <FormControl isRequired>
                     <InputField
                         placeholder="Enter your first name"
                         type="text"
                         labelName="First Name"
                         borderRadius="10"
-                        // value={formState.name}                            
-                        // onChangeText={(value) => setFormState({ ...formState, name: value })}
+                        value={formState.first_name}                            
+                        onChangeText={(value) => setFormState({ ...formState, first_name: value })}
                     />
                     <InputField
                         placeholder="Enter your last name"
                         type="text"
                         labelName="Last Name"
                         borderRadius="10"
-                        // value={formState.name}                            
-                        // onChangeText={(value) => setFormState({ ...formState, name: value })}
+                        value={formState.last_name}                            
+                        onChangeText={(value) => setFormState({ ...formState, last_name: value })}
                     />
                     <InputField
                         placeholder="Enter a unique username(max 20 char)"
                         type="text"
                         labelName="Username"
                         borderRadius="10"
-                        // value={formState.name}                            
-                        // onChangeText={(value) => setFormState({ ...formState, name: value })}
+                        value={formState.username}                            
+                        onChangeText={(value) => setFormState({ ...formState, username: value })}
                     />
                     <InputField
                         placeholder="Enter your email id"
                         type="email"
                         labelName="Email"
                         borderRadius="10"
-                        // value={formState.email}                            
-                        // onChangeText={(value) => setFormState({ ...formState, email: value })}
+                        value={formState.email}                            
+                        onChangeText={(value) => setFormState({ ...formState, email: value })}
                     />  
                     <InputField
                         placeholder="For ex: +91 9898989898"
                         type="tel"
                         labelName="Phone Number"
                         borderRadius="10"
-                        // value={formState.email}                            
-                        // onChangeText={(value) => setFormState({ ...formState, email: value })}
+                        value={formState.phone}                            
+                        onChangeText={(value) => setFormState({ ...formState, phone: value })}
                     />  
                     <InputField
                         placeholder="For ex: NEWUSER"
                         type="text"
                         labelName="Referral Code" 
                         borderRadius="10"
-                        // value={formState.password}                            
-                        // onChangeText={(value) => setFormState({ ...formState, password: value })}                           
+                        value={formState.referral}                            
+                        onChangeText={(value) => setFormState({ ...formState, referral: value })}                           
                     />                                                  
                     <CustomButton
                     bg="purple.700"
-                    // isLoading={true}
+                    isLoading={loading}
                     loadingText="Please wait a moment"
                     loadingBg="purple.700:alpha.70"
                     borderRadius="100" 
                     py={3}                  
-                    onPress={handleModalOverlay}
+                    onPress={handleGetOtp}
                     >
                         Get OTP 
                     </CustomButton>
